60950fd725c7dae8552e5fbd9ae4d78066a078bb
Penoki <pekinohat@gmail.com>
2021-06-05 23:03:09 +0300

    Crapa la adaugat poza dupa adresa din baza de date in Panou.java

--- app/src/main/java/com/example/absolventi/Panou.java
+++ app/src/main/java/com/example/absolventi/Panou.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +36,7 @@ public class Panou extends AppCompatActivity {
 
     private List<String> idList;
     private ImageButton diriginte;
+    private ImageView dirig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,7 @@ public class Panou extends AppCompatActivity {
         final ArrayList<String> list_elevi =new ArrayList<>();
         final ArrayList<String> adresa_dirig =new ArrayList<>();
 
+        dirig=findViewById(R.id.diriguboss);
         diriginte=findViewById(R.id.imageButton);
         final PanouData pd= (PanouData)getIntent().getSerializableExtra("PanouData");
         int y=pd.getSecol();
@@ -58,9 +62,8 @@ public class Panou extends AppCompatActivity {
         x+=r;
 
         //diriginte.setImageURI(Uri.parse("file:///C://DND/Ferd Claymore.png"));
-        diriginte.setImageURI(Uri.parse("https://cdn.drawception.com/drawings/903210/v6MMJO9W74.png"));
-
-
+        //diriginte.setImageURI(Uri.parse("https://www.google.ro/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fwiselies1962%2Fart%2FDeath-on-Vacation-Death-Visits-the-Farm-759166996&psig=AOvVaw2XbdCt3tmJgzbTrKvLeOu8&ust=1623008233476000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjiwf2egfECFQAAAAAdAAAAABAD"));
+        //dirig.setImageDrawable(LoadImageFromWebOperations("https://cdn.drawception.com/drawings/903210/v6MMJO9W74.png"));
 
 
         //1. SELECT * FROM Persoane
@@ -98,6 +101,18 @@ public class Panou extends AppCompatActivity {
 
             }
         });
+
+        //Glide.with(this).load(adresa_dirig.get(0)).into(dirig);
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 
